7ef79f1c423a0b7225d208e5f5d31ca7a4eb92f0
Chris Penny <chris.penny@gmail.com>
2024-07-25 10:57:52 +1200

    Add new extension point. Update docs

--- src/Extension/SearchResultsExtension.php
+++ src/Extension/SearchResultsExtension.php
@@ -6,13 +6,19 @@ use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Extension;
 use SilverStripe\Discoverer\Query\Query;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\Form;
 
 class SearchResultsExtension extends Extension
 {
 
-    public function updateSearchForm(FieldList $fields, FieldList $actions): void
+    public function updateSearchFieldLists(FieldList $fields, FieldList $actions): void
     {
-        // Override this method if you would like to update the form displayed in your template
+        // Override this method if you would like to update the field lists before they are added to your search form
+    }
+
+    public function updateSearchForm(Form $form): void
+    {
+        // Override this method if you would like to update the search form before it is sent to the template
     }
 
     public function updateSearchQuery(Query $query, HTTPRequest $request): void
